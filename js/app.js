6109a5318e3c8ece6dc08dce88c38d198686e32c
Mark Guerra <markgx@gmail.com>
2011-08-31 23:57:55 -0700

    initial work on adding

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-var app = { };
+var DoDoContainerView = Backbone.View.extend({
+  events: {
+    'keyup #new-task-field': 'newTask'
+  },
+
+  newTask: function(e) {
+    if (e.keyCode !== 13) {
+      return;
+    }
+
+    var $newTaskField = $(e.currentTarget);
+    var li = $('#todo-list-item').tmpl({ taskText: $newTaskField.val() });
+    $('#todos-list').append(li);
+    $newTaskField.val('');
+
+    // TODO: persist
+  }
+});
+
+window.app = {
+  views: {},
+
+  init: function() {
+    app.views.containerView = new DoDoContainerView({ el: $('#dodo-container') });
+  }
+};
