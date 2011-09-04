e29913a765b5a1442095df5cd05f0e141eda472b
Mark Guerra <markgx@gmail.com>
2011-09-03 17:39:53 -0700

    wip

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
+var Todo = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      completed: false,
+      dateCreated: new Date
+    };
+  }
+});
+
+var Todos = Backbone.Collection.extend({
+  model: Todo,
+  localStorage: new Store("todos")
+})
+
+var TodoView = Backbone.View.extend({
+  tagName: 'li',
+
+  events: {
+    'click .completed': 'clickCompleted'
+  },
+
+  render: function() {
+    $(this.el).html($('#todo-list-item').tmpl({ description: this.model.get('description') }));
+  },
+
+  clickCompleted: function(e) {
+    // TODO: set completed
+    alert('TODO');
+  },
+});
+
 var DoDoContainerView = Backbone.View.extend({
+  initialize: function() {
+    app.todos.bind('add', this.addTodo, this);
+    app.todos.bind('reset', this.addAll, this);
+  },
+
   events: {
     'keyup #new-task-field': 'newTask'
   },
@@ -10,11 +46,17 @@ var DoDoContainerView = Backbone.View.extend({
       return;
     }
 
-    var li = $('#todo-list-item').tmpl({ taskText: $newTaskField.val() });
-    $('#todos-list').append(li);
-    $newTaskField.val('');
+    var todo = new Todo({ description: $newTaskField.val() });
+    var todoView = new TodoView({ model: todo });
+    todoView.render();
 
-    // TODO: persist
+    app.todos.create(todo);
+
+    $newTaskField.val('');
+  },
+
+  addTodo: function(todo) {
+    $('#todos-list').append(todoView.el);
   }
 });
 
@@ -22,6 +64,7 @@ window.app = {
   views: {},
 
   init: function() {
+    app.todos = new Todos;
     app.views.containerView = new DoDoContainerView({ el: $('#dodo-container') });
   }
 };
